29a71755c493ffcb88f9907ca49fddaf583851cb
vfloressduoc <val.floress@duocuc.cl>
2025-04-11 16:58:10 -0400

    update hooks

--- hooks/productos/useDeleteProducto.jsx
+++ hooks/productos/useDeleteProducto.jsx
@@ -1,13 +1,21 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import useUserStore from "../../stores/userStore";
 
 function useDeleteProducto(options = {}) {
   const queryClient = useQueryClient();
+  const { userToken, selectedAlmacen } = useUserStore();
 
   const deleteProducto = async (id) => {
     const response = await fetch(
       `${import.meta.env.VITE_BACK_URL}api/productos/${id}`,
       {
         method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${userToken}`,
+        },
+        body: JSON.stringify({
+          almacen_id: selectedAlmacen?.id,
+        }),
       }
     );
 
